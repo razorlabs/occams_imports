2544879f33746c2ba231bcb811b9f58fa7b274fe
Jim Krooskos <jkrooskos@ucsd.edu>
2015-07-30 15:43:41 -0700

    Rename routes for codebooks

--- occams_imports/routes.py
+++ occams_imports/routes.py
@@ -11,10 +11,12 @@ def includeme(config):
     Helper method to configure available routes for the application
     """
 
-    config.add_static_view('/imports/static',               'occams_imports:static', cache_max_age=3600)
+    config.add_static_view('/imports/static',               'occams_imports:static',       cache_max_age=3600)
 
-    config.add_route('imports.main',                        '/imports',                              factory=models.RootFactory)
+    config.add_route('imports.main',                        '/imports',                    factory=models.RootFactory)
 
-    config.add_route('imports.upload',                      '/imports/upload',                       factory=models.RootFactory)
+    config.add_route('imports.codebooks_iforms',            '/imports/codebooks-iform',    factory=models.RootFactory)
+
+    config.add_route('imports.codebooks_occams',            '/imports/codebooks-occams',   factory=models.RootFactory)
 
     log.debug('Routes configured')
