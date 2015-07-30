f14b02ff26fe9700afee8577a263470c5e56c436
Jim Krooskos <jkrooskos@ucsd.edu>
2015-07-30 16:36:43 -0700

    Add views for codebook uploads

--- occams_imports/views/codebooks.py
+++ occams_imports/views/codebooks.py
@@ -59,9 +59,117 @@ def get_choices(raw_choices):
 
 
 @view_config(
-    route_name='imports.upload',
+    route_name='imports.codebooks_occams',
     permission='view',
-    renderer='../templates/upload/upload.pt')
+    renderer='../templates/codebooks/occams_codebook.pt')
+def insert_occams(context, request):
+    import sys
+    import argparse
+
+    from occams import Session
+    from occams_datastore import models as datastore
+    from occams_forms.views.field import FieldFormFactory
+    from occams.utils.forms import wtferrors
+    from occams_imports.parsers import parse
+    """
+    Insert appropriate records to the database
+
+    :param records: a list of lists of records from the csv
+    :param: force:  a mode where records are inserted into the db even if
+                    some records are bypassed because they are invalid
+    :param: dry:    no records are inserted into the db.  this is for testing
+
+    :return:  errors dictionary of invalid form datastore.
+              these are the records not inserted
+    """
+    # records, force, dry
+
+    if request.method == 'GET':
+        return {}
+
+    codebook = '/Users/jkrooskos/Documents/occams/src/occams_imports/occams_imports/views/output.csv'
+    records = parse.parse(codebook)
+    records = parse.remove_system_entries(records)
+
+    force = True
+    dry = None
+
+    schemas = {}
+    errors = []
+    for record in records:
+        # if it's not a system entry, cache and insert into db
+        # from pdb import set_trace; set_trace()
+        if (
+            record['schema_name'],
+            record['schema_title'],
+            record['publish_date']
+        ) not in schemas:
+            # create schema object
+            schema = datastore.Schema(
+                name=record['schema_name'],
+                title=record['schema_title'],
+                publish_date=record['publish_date']
+            )
+            Session.add(schema)
+            Session.flush()
+
+            # add schema to cache
+            schemas[(record['schema_name'],
+                    record['schema_title'],
+                    record['publish_date'])] = schema
+
+        else:
+            # get schemas from the cache
+            schema = schemas[(record['schema_name'],
+                              record['schema_title'],
+                              record['publish_date'])]
+
+        # convert boolean type to choice type
+        # occams doesn't support boolean form attribute types
+        if record['type'] == u'boolean':
+            record['type'] = u'choice'
+
+        choices = get_choices(record['choices'])
+        record['choices'] = choices
+
+        FieldForm = FieldFormFactory(context=schema, dbsession=Session)
+        form = FieldForm.from_json(record)
+
+        if form.validate():
+            Session.add(datastore.Attribute(
+                name=record['name'],
+                title=record['title'],
+                description=record['description'],
+                is_required=record['is_required'],
+                is_collection=record['is_collection'],
+                is_private=record['is_private'],
+                type=record['type'],
+                order=record['order'],
+                schema=schema,
+                choices=choices
+            ))
+
+        else:
+            output = {}
+            output['errors'] = wtferrors(form)
+            output['form'] = record
+            errors.append(output)
+
+        if dry:
+            Session.rollback()
+        elif force:
+            Session.flush()
+        elif errors:
+            Session.rollback()
+        else:
+            Session.flush()
+
+    return {}
+
+@view_config(
+    route_name='imports.codebooks_iform',
+    permission='view',
+    renderer='../templates/codebooks/iform_codebook.pt')
 def insert(context, request):
     import sys
     import argparse
