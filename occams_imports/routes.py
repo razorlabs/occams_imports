81a34e72dc14b1b92a409b4e77abd42b869a0511
Jim Krooskos <jkrooskos@ucsd.edu>
2015-07-30 17:11:36 -0700

    Add routes for file uploads

--- occams_imports/routes.py
+++ occams_imports/routes.py
@@ -11,12 +11,16 @@ def includeme(config):
     Helper method to configure available routes for the application
     """
 
-    config.add_static_view('/imports/static',               'occams_imports:static',       cache_max_age=3600)
+    config.add_static_view('/imports/static',               'occams_imports:static',              cache_max_age=3600)
 
-    config.add_route('imports.main',                        '/imports',                    factory=models.RootFactory)
+    config.add_route('imports.main',                        '/imports',                           factory=models.RootFactory)
 
-    config.add_route('imports.codebooks_iform',            '/imports/codebooks/iform',    factory=models.RootFactory)
+    config.add_route('imports.codebooks_iform',             '/imports/codebooks/iform',           factory=models.RootFactory)
 
-    config.add_route('imports.codebooks_occams',            '/imports/codebooks/occams',   factory=models.RootFactory)
+    config.add_route('imports.codebooks_occams',            '/imports/codebooks/occams',          factory=models.RootFactory)
+
+    config.add_route('imports.codebooks_iform_status',      '/imports/codebooks/iform/status',    factory=models.RootFactory)
+
+    config.add_route('imports.codebooks_occams_status',     '/imports/codebooks/occams/status',   factory=models.RootFactory)
 
     log.debug('Routes configured')
