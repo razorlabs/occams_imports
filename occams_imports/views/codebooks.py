e71bb5ed491c043de5b4f670e40f8c92ecb0ee81
Jim Krooskos <jkrooskos@ucsd.edu>
2015-08-07 15:28:10 -0700

    Add forms for iForm upload

--- occams_imports/views/codebooks.py
+++ occams_imports/views/codebooks.py
@@ -110,6 +110,8 @@ def insert_iform(context, request):
 
     codebook = request.POST['codebook'].file
 
+    forms = []
+
     if request.path_info == u'/imports/codebooks/iform/status':
         schema_name = request.POST['schema_name']
         schema_title = request.POST['schema_title']
@@ -117,6 +119,12 @@ def insert_iform(context, request):
         publish_date = request.POST['publish_date']
         publish_date = datetime.strptime(publish_date, '%Y-%m-%d').date()
 
+        form = {'name': schema_name,
+                'title': schema_title,
+                'publish_date': publish_date
+                }
+
+        forms.append(form)
         converted_codebook = iform.convert(
             schema_name, schema_title, publish_date, codebook)
 
@@ -237,5 +245,6 @@ def insert_iform(context, request):
          'errors': errors,
          'error_count': error_count,
          'fields_inserted': fields_inserted,
-         'forms_inserted': forms_inserted},
+         'forms_inserted': forms_inserted,
+         'forms': forms},
          request=request)
