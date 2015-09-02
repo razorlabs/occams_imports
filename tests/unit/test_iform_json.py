bac80485bbec976db568dbd52a3866ebae239424
Jim Krooskos <jkrooskos@ucsd.edu>
2015-09-02 10:27:35 -0700

    Add test for convert choices

--- tests/unit/test_iform_json.py
+++ tests/unit/test_iform_json.py
@@ -1,2 +1,9 @@
 from occams_imports.parsers import iform_json
 
+
+def test_convert_choices():
+    choices = [[u'0', 'label1'], [u'1', 'label2']]
+    actual = iform_json.convert_choices(choices)
+    expected = u'0=label1;1=label2'
+
+    assert actual == expected
