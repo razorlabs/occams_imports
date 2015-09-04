81983645bb3f647029bdf2cbdc3f6006b1f20e91
Jim Krooskos <jkrooskos@ucsd.edu>
2015-09-04 16:12:24 -0700

    Add not allowed tests for impmort forms

--- tests/functional/views/test_imports.py
+++ tests/functional/views/test_imports.py
@@ -4,6 +4,7 @@ from tests.conftest import make_environ, USERID, get_csrf_token
 
 
 ALLOWED = ['administrator', 'manager']
+NOT_ALLOWED = ['editor', 'reviewer']
 
 
 @pytest.mark.parametrize('group', ALLOWED)
@@ -34,3 +35,33 @@ def test_imports_iform(group, config, app):
     response = app.get(url, extra_environ=environ)
 
     assert response.status_code == 200
+
+
+@pytest.mark.parametrize('group', NOT_ALLOWED)
+def test_imports_occams_not_allowed(group, config, app):
+    url = '/imports/codebooks/occams'
+
+    environ = make_environ(userid=USERID, groups=[group])
+    response = app.get(url, extra_environ=environ, expect_errors=True)
+
+    assert response.status_code == 403
+
+
+@pytest.mark.parametrize('group', NOT_ALLOWED)
+def test_imports_qds_not_allowed(group, config, app):
+    url = '/imports/codebooks/qds'
+
+    environ = make_environ(userid=USERID, groups=[group])
+    response = app.get(url, extra_environ=environ, expect_errors=True)
+
+    assert response.status_code == 403
+
+
+@pytest.mark.parametrize('group', NOT_ALLOWED)
+def test_imports_iform_not_allowed(group, config, app):
+    url = '/imports/codebooks/iform'
+
+    environ = make_environ(userid=USERID, groups=[group])
+    response = app.get(url, extra_environ=environ, expect_errors=True)
+
+    assert response.status_code == 403
