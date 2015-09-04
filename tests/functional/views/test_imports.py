cb550801efad99a98b2d710787a5bf29d8f06d40
Jim Krooskos <jkrooskos@ucsd.edu>
2015-09-04 15:37:03 -0700

    Add imports authorization test

--- tests/functional/views/test_imports.py
+++ tests/functional/views/test_imports.py
@@ -1,3 +1,16 @@
 import pytest
 
 from tests.conftest import make_environ, USERID, get_csrf_token
+
+
+ALLOWED = ['administrator']
+
+
+@pytest.mark.parametrize('group', ALLOWED)
+def test_imports_occams(group, config, app):
+    url = '/imports/codebooks/occams'
+
+    environ = make_environ(userid=USERID, groups=[group])
+    response = app.get(url, extra_environ=environ)
+
+    assert response.status_code == 200
