d86b0b2998a7a96711856609ddd813366c948686
Jim Krooskos <jkrooskos@ucsd.edu>
2015-09-04 15:46:52 -0700

    Add tests for qds and iform

--- tests/functional/views/test_imports.py
+++ tests/functional/views/test_imports.py
@@ -3,12 +3,32 @@ import pytest
 from tests.conftest import make_environ, USERID, get_csrf_token
 
 
-ALLOWED = ['administrator']
+ALLOWED = ['administrator', 'manager']
 
 
 @pytest.mark.parametrize('group', ALLOWED)
 def test_imports_occams(group, config, app):
     url = '/imports/codebooks/occams'
+
+    environ = make_environ(userid=USERID, groups=[group])
+    response = app.get(url, extra_environ=environ)
+
+    assert response.status_code == 200
+
+
+@pytest.mark.parametrize('group', ALLOWED)
+def test_imports_qds(group, config, app):
+    url = '/imports/codebooks/qds'
+
+    environ = make_environ(userid=USERID, groups=[group])
+    response = app.get(url, extra_environ=environ)
+
+    assert response.status_code == 200
+
+
+@pytest.mark.parametrize('group', ALLOWED)
+def test_imports_iform(group, config, app):
+    url = '/imports/codebooks/iform'
 
     environ = make_environ(userid=USERID, groups=[group])
     response = app.get(url, extra_environ=environ)
