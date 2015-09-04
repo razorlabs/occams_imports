ddc987e4432587360108a32a9cccf5a80f4fc76c
Jim Krooskos <jkrooskos@ucsd.edu>
2015-09-04 16:15:56 -0700

    Re order tests

--- tests/functional/views/test_imports.py
+++ tests/functional/views/test_imports.py
@@ -17,26 +17,6 @@ def test_imports_occams(group, config, app):
     assert response.status_code == 200
 
 
-@pytest.mark.parametrize('group', ALLOWED)
-def test_imports_qds(group, config, app):
-    url = '/imports/codebooks/qds'
-
-    environ = make_environ(userid=USERID, groups=[group])
-    response = app.get(url, extra_environ=environ)
-
-    assert response.status_code == 200
-
-
-@pytest.mark.parametrize('group', ALLOWED)
-def test_imports_iform(group, config, app):
-    url = '/imports/codebooks/iform'
-
-    environ = make_environ(userid=USERID, groups=[group])
-    response = app.get(url, extra_environ=environ)
-
-    assert response.status_code == 200
-
-
 @pytest.mark.parametrize('group', NOT_ALLOWED)
 def test_imports_occams_not_allowed(group, config, app):
     url = '/imports/codebooks/occams'
@@ -47,6 +27,16 @@ def test_imports_occams_not_allowed(group, config, app):
     assert response.status_code == 403
 
 
+@pytest.mark.parametrize('group', ALLOWED)
+def test_imports_qds(group, config, app):
+    url = '/imports/codebooks/qds'
+
+    environ = make_environ(userid=USERID, groups=[group])
+    response = app.get(url, extra_environ=environ)
+
+    assert response.status_code == 200
+
+
 @pytest.mark.parametrize('group', NOT_ALLOWED)
 def test_imports_qds_not_allowed(group, config, app):
     url = '/imports/codebooks/qds'
@@ -55,6 +45,16 @@ def test_imports_qds_not_allowed(group, config, app):
     response = app.get(url, extra_environ=environ, expect_errors=True)
 
     assert response.status_code == 403
+
+
+@pytest.mark.parametrize('group', ALLOWED)
+def test_imports_iform(group, config, app):
+    url = '/imports/codebooks/iform'
+
+    environ = make_environ(userid=USERID, groups=[group])
+    response = app.get(url, extra_environ=environ)
+
+    assert response.status_code == 200
 
 
 @pytest.mark.parametrize('group', NOT_ALLOWED)
