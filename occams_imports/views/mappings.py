596e8f0fad4b7a5dc14f726ca515b44ab4dde8a1
Jim Krooskos <jkrooskos@ucsd.edu>
2015-09-18 14:03:17 -0700

    Add schema data API

--- occams_imports/views/mappings.py
+++ occams_imports/views/mappings.py
@@ -6,7 +6,6 @@ from pyramid.renderers import render_to_response
 from pyramid.view import view_config
 from pyramid.session import check_csrf_token
 
-from occams.utils.forms import wtferrors
 from .. import Session
 
 
@@ -17,3 +16,29 @@ from .. import Session
     renderer='../templates/mappings/mappings.pt')
 def occams(context, request):
     return {}
+
+
+@view_config(
+    route_name='imports.schemas',
+    permission='view',
+    request_method='GET',
+    renderer='json')
+def get_schemas(context, request):
+    import json
+    from occams_datastore import models as datastore
+
+    schemas = Session.query(datastore.Schema).all()
+
+    data = {}
+    for schema in schemas:
+        try:
+            attributes = schema.attributes.keys()
+        except KeyError:
+            attributes = []
+        data[schema.name] = {
+            u'name': schema.name,
+            u'publish_date': schema.publish_date.strftime('%Y-%m-%d'),
+            u'attributes': attributes
+        }
+
+    return json.dumps(data)
