35fc8c54336eb7d4ed4292fe6837f4a43e4e810d
Jim Krooskos <jkrooskos@ucsd.edu>
2015-10-01 13:38:49 -0700

    Sort choices before template rendering

--- occams_imports/views/view.py
+++ occams_imports/views/view.py
@@ -51,6 +51,8 @@ def get_schemas(context, request):
     request_method='GET',
     renderer='../templates/mappings/mapped.pt')
 def get_schemas_mapped(context, request):
+    from operator import itemgetter
+
     from occams_datastore import models as datastore
     from occams_imports import models as models
 
@@ -73,8 +75,10 @@ def get_schemas_mapped(context, request):
         drsc_variable = mappings.mapped['drsc_variable']
         if mappings.schema.attributes[drsc_variable].type == u'choice':
             choices = mappings.schema.attributes[drsc_variable].choices
+           # from pdb import set_trace; set_trace()
+            # choices = sorted(choices, key=itemgetter('order'))
             # data to populate drsc table
-            for choice in choices:
+            for choice in sorted(choices, key=lambda choice: choices[choice].order):
                 drsc_form_rows.append({
                     'variable': drsc_variable,
                     'description': mappings.schema.title,
@@ -87,8 +91,9 @@ def get_schemas_mapped(context, request):
 
             # We need all choices to diplay even if not mapped
             choices = attribute.choices
+            # choices = sorted(choices, key=itemgetter('order'))
             mappings_form_rows = []
-            for choice in choices:
+            for choice in sorted(choices, key=lambda choice: choices[choice].order):
                 mapped_value = u''
                 mapped_label = u''
                 for row in mappings.mapped['mapping']['choices_map']:
