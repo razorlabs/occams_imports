8a2990020e2845e17ca968a68c734a450b9c7d36
Jim Krooskos <jkrooskos@ucsd.edu>
2015-10-14 16:11:29 -0700

    Add drsc forms to imputation panel

--- occams_imports/static/scripts/imputations.js
+++ occams_imports/static/scripts/imputations.js
@@ -1,3 +1,47 @@
+function choiceModel(name, label){
+  'use strict'
+
+  var self = this;
+
+  self.name = ko.observable(name);
+  self.label = ko.observable(label);
+  self.mapped = ko.observable('');
+}
+
+function attributeModel(variable, label, choices){
+  'use strict'
+
+  var self = this;
+
+  self.variable = ko.observable(variable);
+  self.label = ko.observable(label);
+  self.choices = ko.observableArray([]);
+
+  var choicesLength = choices.length;
+
+  for (var i = 0; i < choicesLength; i++){
+    self.choices.push(new choiceModel(choices[i].name, choices[i].label));
+  }
+}
+
+function formModel(name, publish_date, attributes){
+  'use strict'
+
+  var self = this;
+
+  self.name = ko.observable(name);
+  self.publish_date = ko.observable(publish_date);
+  self.attributes = ko.observableArray([]);
+
+  var attributeLength = attributes.length;
+
+  for (var i = 0; i < attributeLength; i++){
+    self.attributes.push(
+      new attributeModel(
+        attributes[i].variable, attributes[i].label, attributes[i].choices));
+  }
+}
+
 function comparisonModel(){
   'use strict';
 
@@ -41,6 +85,13 @@ function imputationViewModel(){
   self.logicalOperators = ko.observableArray([]);
   self.confidence = ko.observable(1);
 
+  self.isReady = ko.observableArray(false);
+  self.isLoading = ko.observable(true);
+
+  self.drsc_forms = ko.observableArray();
+  self.selectedDRSCForm = ko.observable();
+  self.selectedDRSCAttribute = ko.observable();
+
   self.addLogicalOperator = function(){
     self.logicalOperators.push(new logicalModel());
 
@@ -79,5 +130,30 @@ function imputationViewModel(){
                           comparison: self.comparisonOperators()})
     console.log(data);
   }
+
+  $.ajax({
+    url: '/imports/schemas',
+    method: 'GET',
+    headers: {'X-CSRF-Token': $.cookie('csrf_token')},
+    beforeSend: function(){
+      self.isReady(true);
+    },
+    success: function(data, textStatus, jqXHR){
+      var json = $.parseJSON(data);
+
+      $.each(json.forms, function(){
+        var form = new formModel(this.name, this.publish_date, this.attributes);
+        if (this.site != 'DRSC'){
+
+        }
+        else{
+          self.drsc_forms.push(form);
+        }
+      });
+    },
+    complete: function(){
+      self.isLoading(false);
+  }
+  });
 
 }
