8fe8e63fbb77ef01a0b8a48de5653dddc5efa045
Jim Krooskos <jkrooskos@ucsd.edu>
2015-10-14 11:33:19 -0700

    Add log for mapped data

--- occams_imports/static/scripts/imputations.js
+++ occams_imports/static/scripts/imputations.js
@@ -1,11 +1,17 @@
 function comparisonModel(){
   'use strict';
 
+  var self = this;
+
   self.operators = ko.observableArray(['==', '!=', '>', '<', '>=', '<=']);
   self.value = ko.observable();
 }
 
 function logicalModel(){
+  'use strict'
+
+  var self = this;
+
   self.operators = ko.observableArray(['and', 'or']);
 }
 
@@ -25,15 +31,37 @@ function imputationViewModel(){
   }
 
   self.addComparisonOperator = function(){
+    'use strict'
+
+    var self = this;
+
     self.comparisonOperators.push(new comparisonModel());
   }
 
   self.deleteComparisonOperator = function(){
+    'use strict'
+
+    var self = this;
+
     self.comparisonOperators.pop()
   }
 
   self.deleteLogicalOperator = function(){
+    'use strict'
+
+    var self = this;
+
     self.logicalOperators.pop()
+  }
+
+  self.saveImputation = function(){
+    'use strict'
+
+    var self = this;
+
+    var data = ko.toJSON({logical: self.logicalOperators(),
+                          comparison: self.comparisonOperators()})
+    console.log(data);
   }
 
 }
