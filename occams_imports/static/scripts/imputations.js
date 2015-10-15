35c6e26d71167c44faec327482583c40941f5de8
Jim Krooskos <jkrooskos@ucsd.edu>
2015-10-15 11:09:46 -0700

    Add drsc data to ko.JSON

--- occams_imports/static/scripts/imputations.js
+++ occams_imports/static/scripts/imputations.js
@@ -130,7 +130,10 @@ function imputationViewModel(){
 
     var data = ko.toJSON({logical: self.logicalOperators(),
                           comparison: self.comparisonOperators(),
-                          selected_comparison_condition: self.selectedComparisonCondition})
+                          selected_comparison_condition: self.selectedComparisonCondition,
+                          drsc_form: self.selectedDRSCForm().name,
+                          drsc_publish_date: self.selectedDRSCForm().publish_date,
+                          drsc_variable: self.selectedDRSCAttribute().variable})
     console.log(data);
   }
 
