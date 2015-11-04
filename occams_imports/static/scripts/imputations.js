332f2d42f54d2969805ab6b10c8995fb8173fc12
Jim Krooskos <jkrooskos@ucsd.edu>
2015-11-04 15:30:16 -0800

    Add label tooltip over datatype

--- occams_imports/static/scripts/imputations.js
+++ occams_imports/static/scripts/imputations.js
@@ -1,3 +1,23 @@
+ko.bindingHandlers.tooltip = {
+    init: function(element, valueAccessor) {
+        var local = ko.utils.unwrapObservable(valueAccessor()),
+            options = {};
+
+        ko.utils.extend(options, ko.bindingHandlers.tooltip.options);
+        ko.utils.extend(options, local);
+
+        $(element).tooltip(options);
+
+        ko.utils.domNodeDisposal.addDisposeCallback(element, function() {
+            $(element).tooltip("destroy");
+        });
+    },
+    options: {
+        placement: "right",
+        trigger: "hover"
+    }
+};
+
 function choiceImputationModel(name, label){
   'use strict'
 
