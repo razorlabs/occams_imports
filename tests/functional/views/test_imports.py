ce5b41edbe7fcd5659918448cc5d4db31fea435d
Jim Krooskos <jkrooskos@ucsd.edu>
2015-12-04 15:15:11 -0800

    Add tests for cytoscape and fix jointjs tests

--- tests/functional/views/test_imports.py
+++ tests/functional/views/test_imports.py
@@ -363,16 +363,34 @@ class TestImports:
         assert response.status_code == 401
 
     @pytest.mark.parametrize('group', ALLOWED)
-    def test_mappings_imputation_demo_allowed(self, app, group):
-        url = '/imports/mappings/imputation/demo'
+    def test_jointjs_demo_allowed(self, app, group):
+        url = '/imports/demos/jointjs'
 
         environ = make_environ(userid=USERID, groups=[group])
         response = app.get(url, extra_environ=environ)
 
         assert response.status_code == 200
 
-    def test_mappings_imputation_demo_not_authenticated(self, app):
-        url = '/imports/mappings/imputation/demo'
+    def test_jointjs_demo_not_authenticated(self, app):
+        url = '/imports/demos/jointjs'
+
+        response = app.get(
+            url,
+            status='*')
+
+        assert response.status_code == 401
+
+    @pytest.mark.parametrize('group', ALLOWED)
+    def test_cytoscape_demo_allowed(self, app, group):
+        url = '/imports/demos/cytoscapejs'
+
+        environ = make_environ(userid=USERID, groups=[group])
+        response = app.get(url, extra_environ=environ)
+
+        assert response.status_code == 200
+
+    def test_cytoscape_demo_not_authenticated(self, app):
+        url = '/imports/demos/cytoscapejs'
 
         response = app.get(
             url,
