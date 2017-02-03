a7ff3b1818920f68cc62bf8311c8fea3cfc7b6f5
Jim Krooskos <jkrooskos@ucsd.edu>
2017-02-03 10:09:44 -0800

    Add uploads and project-upload-list

--- client/app.js
+++ client/app.js
@@ -8,6 +8,8 @@ import 'containers/direct-map'
 import 'containers/impute-map'
 import 'containers/project-list'
 import 'containers/mapped'
+import 'containers/uploads'
+import 'containers/project-upload-list'
 
 import './app.scss'
 
