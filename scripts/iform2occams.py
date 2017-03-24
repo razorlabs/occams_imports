2b4a3fe0be2095952bb60a43e662c93d849f61cd
Marco Martinez <m-martinez@users.noreply.github.com>
2017-03-24 16:56:21 -0700

    Implement command line interface for iForm data dictionary converter

---
+++ scripts/iform2occams.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+
+"""
+This script consolidates iForm data dictionaries into single version eCRFs
+"""
+
+import os
+import fnmatch
+import sys
+
+import click
+
+from occams_imports.parsers.iform_json import convert as convert_iform
+
+
+def glob_json(path):
+    """
+    Recursively iterate through all JSON files in a directory
+    """
+    for __, __, filenames in os.walk(path):
+        for filename in fnmatch.filter(filenames, '*.json'):
+            yield filename
+
+
+@click.command()
+@click.option(
+    '--src',
+    type=click.Path(exists=True),
+    help='Input iForm data dictionary directory or file'
+)
+@click.option(
+    '--dst',
+    type=click.File('wb'),
+    default=sys.stdout,
+    help='(Optional) Output codebook file name, otherwise prints to STDOUT'
+)
+def main(src, dst):
+    """
+    Converter script of iForm data dictionaries to OCCAMS code books
+    """
+
+    if os.path.isdir(src):
+        filenames = glob_json(src)
+
+    elif os.path.isfile(src):
+        src, basename = os.path.split(src)
+        filenames = [basename]
+
+    else:
+        raise Exception('Unkown source file type: %s' % src)
+
+    header = None
+
+    for filename in filenames:
+
+        with open(os.path.join(src, filename), 'rb') as in_buffer:
+            parsed_buffer = convert_iform(in_buffer)
+
+            firstline = parsed_buffer.readline()
+
+            # Only write the header for the first file processed
+            if not header:
+                header = firstline
+                dst.write(firstline)
+
+            for line in parsed_buffer:
+                dst.write(line)
+
+            parsed_buffer.close()
+
+    dst.close()
+
+
+if __name__ == '__main__':
+    main()
